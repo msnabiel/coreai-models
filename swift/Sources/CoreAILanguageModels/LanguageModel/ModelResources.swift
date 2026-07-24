1e2a74ee6f2e115ed49b89b321b91a425614ca6b
msnabiel <89602212+msnabiel@users.noreply.github.com>
2026-07-24 21:15:19 +0530

    Guard model resources on simulator

--- swift/Sources/CoreAILanguageModels/LanguageModel/ModelResources.swift
+++ swift/Sources/CoreAILanguageModels/LanguageModel/ModelResources.swift
@@ -3,6 +3,7 @@
 // Use of this source code is governed by a BSD-3-clause license that can
 // be found in the LICENSE file or at https://opensource.org/licenses/BSD-3-Clause
 
+#if canImport(CoreAI)  // canImport-CoreAI sim guard: CoreAILanguageModel is device-only
 import CoreAIShared
 import Foundation
 import Synchronization
@@ -161,3 +162,4 @@ final class ModelResources: ResourceManaging {
         return engine
     }
 }
+#endif
